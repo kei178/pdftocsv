b526371d9bcc33f1f13c7c83b1fdb816ec099a4a
Kei178 <keisuke.inaba.cpa@gmail.com>
2021-01-20 17:11:21 -0600

    Initial commit

---
+++ spec/pdftocsv_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pdftocsv do
+  it "has a version number" do
+    expect(Pdftocsv::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
